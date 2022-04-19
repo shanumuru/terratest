355ed4e735498fef931289ee2bdae8b31b1d4ebc
shanumuru <shanmugapriya84@gmail.com>
2022-04-19 16:20:47 -0700

    jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,26 @@
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+            git branch: 'main', credentialsId: '34b3cc00-d1e9-4d72-9172-599c899b28e1', url: 'https://github.com/shanumuru/terratest'
+
+          }
+        }
+        
+        stage ("terraform init") {
+            steps {
+                sh ('terraform init') 
+            }
+        }
+        
+        stage ("terraform apply ") {
+            steps {
+          
+                sh ('terraform apply --auto-approve') 
+           }
+        }
+    }
+}
